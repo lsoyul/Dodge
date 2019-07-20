b13735911eb4ae7b811e4e906930cf31e187dfce
sgykim15 <sgykim36@gmail.com>
2019-07-20 22:08:19 +0900

    Initial commit

---
+++ Assets/Logics/GlobalVariables/GlobalVariables.cs
@@ -0,0 +1,19 @@
+﻿
+namespace GlobalVariables
+{
+    public enum GroundType
+    {
+        NORMAL,
+        DANGER,
+        INSTANTLY_KILL,
+        PORTAL,
+        START_POINT,
+        FINISH_POINT
+    }
+
+    public enum CameraMode
+    {
+        FOLLOW,
+        ENTIRE_VIEW
+    }
+}
